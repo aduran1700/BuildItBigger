8ef2684f7206aa58b62a0a3cbdc649c9d881b50f
aduran <aduran1700@gmail.com>
2016-07-11 14:07:23 -0400

    Initial commit

---
+++ javajokes/src/main/java/com/java/Jokes.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class Jokes {
+    private String joke;
+
+    public Jokes(String joke) {
+        this.joke = joke;
+    }
+
+    public String getJoke() {
+        return this.joke;
+    }
+}
